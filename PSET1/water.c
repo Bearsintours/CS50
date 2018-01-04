0fc251bfaf36384b52a811782f15384c5dc492fa
Quentin <qbrogly@gmail.com>
2018-01-03 21:00:03 -0800

    create water.c

---
+++ PSET1/water.c
@@ -0,0 +1,15 @@
+#import <stdio.h>
+#import <cs50.h>
+
+int main(void){
+    printf("Minutes:\n");
+    int minutes;
+    do{
+      minutes = get_int();
+    }
+    while(minutes < 0);
+
+    int bottles = minutes*12;
+    printf("Minutes: %i\n", minutes);
+    printf("Bottles: %i\n", bottles);
+}
